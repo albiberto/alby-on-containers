b5864307239cae12159340ee38ce46121e3949ef
Alberto Viezzi <aviezzi@outlook.com>
2024-04-16 23:56:54 +0200

    Add categories

---
+++ ProductDataManager/Components/Pages/Categories.razor.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.EntityFrameworkCore;
+using MudBlazor;
+using ProductDataManager.Infrastructure;
+using ProductDataManager.Infrastructure.Domain;
+
+
+namespace ProductDataManager.Components.Pages;
+
+public partial class Categories
+{
+    [Inject] private ProductContext Context { get; set; } = null!;
+    
+    private List<Category> Elements = [];
+
+    
+
+    protected override async Task OnInitializedAsync()
+    {
+        Elements = await Context.Categories
+            .Where(c => c.ParentId == null)
+            .Include(c => c.Categories)
+            .ThenInclude(c => c.Categories)
+            .ToListAsync();
+    }
+}
